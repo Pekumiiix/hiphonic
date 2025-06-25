09aa419cdbeccd2f8ba25a5fd1b282c400388fbc
pelumi-nirodigital <113313095+Pekumiiix@users.noreply.github.com>
2025-06-25 18:05:07 +0100

    Initial Commit

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Hiphonic",
+  description: "Project Manager",
+  icons: "/assets/logo.png",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
