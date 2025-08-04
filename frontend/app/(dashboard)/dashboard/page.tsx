30ff16264e4a660ddb0421c1b86af8631e0e471b
pelummie <amaopelumi96@gmail.com>
2025-08-04 17:08:25 +0100

    Update page.tsx

--- frontend/app/(dashboard)/dashboard/page.tsx
+++ frontend/app/(dashboard)/dashboard/page.tsx
@@ -53,7 +53,7 @@ export default function DashboardPage() {
       <DashboardNav />
 
       <section className='w-full grid grid-cols-2 xl:grid-cols-3 gap-6 px-4 md:px-8 py-6'>
-        <div className='w-full h-fit col-span-2 flex flex-col md:grid grid-col-2 justify-center gap-6'>
+        <div className='w-full h-fit col-span-2 flex flex-col md:grid grid-col-2 max-md:justify-center gap-6'>
           <RecentProjects />
           <ActivitySummary />
           <TaskSummary />
